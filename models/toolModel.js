07601b1765f51277f7f7bf9b737b5c9d5c8954e6
yasey <ydeeyagahawadug@deakin.edu.au>
2021-08-19 16:16:12 +1000

    Create tool schema

--- models/toolModel.js
+++ models/toolModel.js
@@ -2,9 +2,36 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// create new database schema
-const toolSchema = new Schema({
+// create geolocation Schema
+const GeoSchema = new Schema({
+    type: {
+        type: String,
+        default: 'Point'
+    },
+    coordinates: {
+        type: [Number],
+        index: '2dsphere'
+    }
+});
 
+// create tool schema
+const toolSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    size: {
+        type: String
+    },
+    available: {
+        type: Boolean,
+        default: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    location: GeoSchema
 });
 
 // create and export tool instance
