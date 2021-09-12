ec89bff3692de52ca84b80b2d185c8dc898a3db2
yaseyomz <yasey94@gmail.com>
2021-09-12 22:50:41 +1000

    Google oauth2.0 backend functionality implemented

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,7 +18,9 @@ const getSignup = (req, res) => {
 // log in a user
 const postLogin = (req, res) => {
     if (req.isAuthenticated()) {
-        res.send({ user: req.user });
+        console.log(req.user);
+        console.log(req.session);
+        res.redirect("/");
     } else {
         res.redirect("/users/login");
     }
@@ -64,12 +66,69 @@ const getLogout = (req, res) => {
     res.redirect('/users/login');
 }
 
+// authenticate google user
+const postGoogleLogin = async (req, res) => {
+    try {
+        const code = req.body.code;
+        const profile = await googleOAuth.getProfile(code);
+        const user = {
+            id: profile.sub,
+            name: profile.name,
+            email: profile.email,
+        };
+    
+        // adding google user to the database
+        User.findOne({ email: user.email }).then((dbUser) => {
+            if (dbUser) {
+                req.user = dbUser;
+                req.session.passport = { user: dbUser.id };
+                console.log("This user already exists");
+                console.log(req.user);
+                console.log(req.session);
+            } else {
+                dbUser = {
+                    name: user.name,
+                    email: user.email,
+                    password: user.id,
+                };
+        
+                const newUser = new User(dbUser);
+        
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(dbUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        newUser.password = hash;
+            
+                        newUser.save().then((user) => {
+                            req.user = user;
+                            req.session.passport = { user: user.id };
+                            console.log("Google user has been added to the database");
+            
+                            console.log(req.user);
+                            console.log(req.session);
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                        });
+                    });
+                });
+            }
+        });
+    
+        res.redirect("/");
+      } catch (err) {
+        console.log(err);
+        res.status(401).send();
+      }
+}
+
 // export user controllers
 userController = {
     getLogin,
     getSignup,
     postLogin,
     postSignup,
-    getLogout
+    getLogout,
+    postGoogleLogin
 };
 module.exports = userController;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,12 +8,13 @@ const userController = require('../controllers/userController');
 // create express router
 const router = express.Router();
 
-// user routes
+// user auth routes
 router.get('/login', userController.getLogin);
 router.get('/signup', userController.getSignup);
 router.post('/login', passport.authenticate("local"), userController.postLogin);
 router.get('/logout', userController.getLogout);
 router.post('/signup', userController.postSignup);
+router.post('/auth/google', userController.postGoogleLogin);
 
 // export router handles
 module.exports = router;
